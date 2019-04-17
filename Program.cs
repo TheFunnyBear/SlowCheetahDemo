0a010a2f5c814dfbae8be81c446cd3cd40f4e046
Denis <40884381+TheFunnyBear@users.noreply.github.com>
2019-04-17 18:12:33 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Configuration;
+
+namespace SlowCheetahDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var configFile = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            NameValueCollection section = (NameValueCollection)ConfigurationManager.GetSection("appSettings");
+            string id = section["userId"];
+            string userName = section["userName"];
+            string userPassword = section["userPassword"];
+            
+            Console.WriteLine("Loaded from configuration:");
+            Console.WriteLine($"Useer Id: [{id}] Name [{userName}] with passsword [{userPassword}].");
+            Console.WriteLine("Press any key.");
+            Console.ReadKey();
+        }
+    }
+}
